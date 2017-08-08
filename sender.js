a50776fd10479f68fbde152ef30159e2cd49a8f6
Hyukpyo Hong <hyukpyohong@gmail.com>
2017-08-08 01:20:18 -0400

    start to web version

--- sender.js
+++ sender.js
@@ -1,10 +1,16 @@
-engine.on('game_crash', function (data) {    
-    $.ajax({
-        url: "ec2-54-204-184-186.compute-1.amazonaws.com:3000/crash",
-        type: "POST",
-        data: {
-            'crash': (data.game_crash / 100),
-        },
-    });
+engine.on('game_crash', function (data) {
+    try{
+    var url = "https://localhost:3000/crash";
+    var params = "crash=" + data.game_crash;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(params);
+    }catch(e){
+        
+    }
+
 });
 
